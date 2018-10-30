005e223956e8fd63385d733f1672826385741567
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-10-30 20:09:06 +0000

    automatically backed up by learn

---
+++ app/models/artist.rb
@@ -0,0 +1,8 @@
+require_all 'app'
+require_all 'lib'
+class Artist < ActiveRecord::Base
+  include Slugifiable::InstanceMethods
+  extend Slugifiable::ClassMethods
+  has_many :songs
+  has_many :genres, through: :songs
+end
---
+++ app/models/song.rb
@@ -0,0 +1,11 @@
+require_all 'app'
+require_all 'lib'
+class Song < ActiveRecord::Base
+  include Slugifiable::InstanceMethods
+  extend Slugifiable::ClassMethods
+
+  belongs_to :artist
+  has_many :song_genres
+  has_many :genres, through: :song_genres
+
+end
